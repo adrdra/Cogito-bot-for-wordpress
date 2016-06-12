c2b7e141635058bc5871c325d6a528f2def37031
ADRDra <alexisHetic>
2016-06-12 12:26:50 +0200

    Add plugin infos

---
+++ cogito-bot.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Cogito Bot
+ */
+ /*
+ Plugin Name: Cogito Bot
+ Plugin URI: http://cogito-bot.com/
+ Description: Used by the best ecommerce websites, Cogito Bot is the best way to <strong>connect your ecommerce to messenger</strong>. It permits to create a more exclusive contact with your consummer.
+ Version: 1.0.0
+ Author: Cogito
+ Author URI: http://cogito-bot.com/
+ License: GPLv2 or later
+ Text Domain: cogito-bot
+ */
