2ffa52a9d533878627d51e4878d4a90ec8e6ca8e
ADRDra <alexisHetic>
2016-06-14 15:45:39 +0200

    create cogito banner

--- views/view.admin-settings.php
+++ views/view.admin-settings.php
@@ -1,5 +1,13 @@
 <div class="cogito-layout">
-  <div class="cogito-banner">
+  <div
+    class="cogito-banner"
+    style="background-image: url(<?php echo COGITO_BOT__PLUGIN_URL . 'assets/img/bandeau.png' ?>)">
 
+    <div
+      class="cogito-logo"
+      style="background-image: url(<?php echo COGITO_BOT__PLUGIN_URL . 'assets/img/logo.png' ?>)">
+
+    </div>
+    <h2 class="center">Améliorer l'expérience de recherche et le taux de succès de votre site e-commerce</h2>
   </div>
 </div>
