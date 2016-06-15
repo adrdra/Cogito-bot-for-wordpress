62f97124c805ce060b5d7ca3056dbf69da269b89
ADRDra <alexisHetic>
2016-06-15 14:37:23 +0200

    Add buy_cart and facebook_auth_redirect routes

--- includes/api/class.cogito-bot-api.php
+++ includes/api/class.cogito-bot-api.php
@@ -7,19 +7,30 @@ class CogitoBot_Api {
   }
 
   public function register_routes() {
-    $namespace = 'cogito/v1';
-    $base = 'route';
+    $namespace = 'cogito-plugin';
+    $base = 'facebook-auth-redirect';
 
-    register_rest_route( $namespace, 'route',
-        array(
-            'methods' => 'GET',
-            'callback'=> array( $this, 'get_response' )
-        )
+    register_rest_route( $namespace, 'facebook-auth-redirect',
+      array(
+          'methods' => 'GET',
+          'callback'=> array( $this, 'facebook_auth_redirect' )
+      )
+    );
+
+    register_rest_route( $namespace, 'buy-cart',
+      array(
+          'methods' => 'GET',
+          'callback'=> array( $this, 'buy_cart' )
+      )
     );
   }
 
-  public function get_response() {
-    return 'nikel';
+  public function facebook_auth_redirect() {
+    return 'facebook_auth_redirect';
+  }
+
+  public function buy_cart() {
+    return 'buy_cart';
   }
 }
 
